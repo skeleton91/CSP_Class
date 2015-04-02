122f0617c2e1551ed3fc0c5ab09ba9f6076a7e66
me <me@me-PC>
2015-04-02 20:25:51 +0900

    ㅇㅡ으ㅡㅡㅡㅡㅡ

---
+++ MyJava/src/myJava/Class001.java
@@ -0,0 +1,13 @@
+package myJava;
+
+class Class002 {
+	Class002() {
+		System.out.println("으아아 왜 안되는거야");
+	}
+}
+
+public class Class001 {
+	public static void main(String[] args) {
+		new Class002();
+	}
+}
